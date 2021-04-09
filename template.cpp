74c42f482ba1f9eab568cd5b4fc367923ef0850f
Ivan Sinek <isinek@gmail.com>
2021-04-09 11:22:48 +0200

    Adding C++ template with debug helpers.

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define dump(...)	cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl
+#define arr_dump(...)	cerr << "[" << #__VA_ARGS__ "(" << sizeof((__VA_ARGS__))/sizeof((__VA_ARGS__)[0]) << "): ["; for (unsigned long long int it = 0; it < sizeof((__VA_ARGS__))/sizeof((__VA_ARGS__)[0]); ++it) cerr << (__VA_ARGS__)[it] << (it + 1 < sizeof((__VA_ARGS__))/sizeof((__VA_ARGS__)[0]) ? ", " : ""); cerr << "]]" << endl
+#define vec_dump(...)	cerr << "[" << #__VA_ARGS__ ": ["; for (auto it = (__VA_ARGS__).begin(); it != (__VA_ARGS__).end(); ++it) cerr << *it << (it + 1 != (__VA_ARGS__).end() ? ", " : ""); cerr << "]]" << endl
+
+using namespace std;
+
+void solution()
+{
+	return;
+}
+
+int main()
+{
+	unsigned int T;
+
+	scanf("%d", &T);
+	for (int i = 1; i <= T; ++i) {
+		printf("Case #%u: ", i);
+		solution();
+	}
+}
